19acbedfcc149fb1ddd33d9790c53872517fc5d2
kiven <kiven.mr@gmail.com>
2015-12-23 17:57:17 +0800

    init

---
+++ rest_arch/__init__.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+  REST_ARCH
+  ~~~~~~~
+
+  RESTful Framework Based On Flask
+"""
+
+version_info = (0, 0, 1)
+__version__ = ".".join([str(v) for v in version_info])
