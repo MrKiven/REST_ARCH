91bc41cdf27cccc047298e2c1b8c196faf1a9744
kiven <kiven.mr@gmail.com>
2015-12-30 16:39:09 +0800

    add base exc add service class

---
+++ rest_arch/exc.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+
+from thriftpy.transport import TTransportException
+
+
+class ServerRestartException(TTransportException):
+    """Raised if a `ping` request incomes but our sever is shutting down."""
+    pass
+
+
+class ArchWarningException(Exception):
+    pass
+
+
+###
+# Message's Exc
+###
+
+class ArchMessageExc(Exception):
+    """ Message base exc """
+
+
+class BrokersInfoNotFoundExc(ArchMessageExc):
+    message = 'Could not found mq\' brokers info in app.yaml'
+
+
+class ConsumerConfigBadFormatExc(ArchMessageExc):
+    message = 'Message consumer\'s config is not correctly formatted ' \
+              'in app.yaml.'
+
+
+class BrokerUrlsNotConfigedExc(ArchMessageExc):
+    message = 'Message queue\'s broker urls is not configured in app.yaml'
