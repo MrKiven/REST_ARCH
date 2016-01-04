32b3c58fe1d584c2aabd3c4752ceb7d563441814
kiven <kiven.mr@gmail.com>
2016-01-04 15:42:13 +0800

    click add command

--- rest_arch/skt/cmds/__init__.py
+++ rest_arch/skt/cmds/__init__.py
@@ -1,1 +1,15 @@
 # -*- coding: utf-8 -*-
+
+import logging
+import click
+
+
+logger = logging.getLogger(__name__)
+
+
+@click.group()
+@click.version_option()
+def skt():
+    pass
+
+skt.add_command()
