7adc8b78a216e0c1c50a3ed37fe0ef9816dfacbb
kiven <kiven.mr@gmail.com>
2016-01-04 15:09:40 +0800

    add if app activated

--- rest_arch/app.py
+++ rest_arch/app.py
@@ -2,10 +2,22 @@
 
 from flask import Flask
 
+app_activated = False
+
 
 ##
 # make flask app with param config
 ##
 def make_app(**config):
-    app = Flask(__name__)
-    return app
+    global app_activated
+    try:
+        app = Flask(**config)
+        return app
+    except Exception:
+        raise SystemExit("app has not activated!")
+    finally:
+        app_activated = True
+
+
+def is_app_activated():
+    return app_activated
