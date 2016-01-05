cf46ac0a4f83bd4311f9b6d49da3f987e77798a4
kiven <kiven.mr@gmail.com>
2016-01-05 16:50:52 +0800

    init env

--- rest_arch/skt/runner/hooks.py
+++ rest_arch/skt/runner/hooks.py
@@ -6,8 +6,6 @@ Hooks around gunicorn runner.
 
 import os
 
-from . import signal
-
 
 def worker_term(worker):
     os.environ['SERVER_SHUTTINGDOWN'] = '1'
@@ -23,15 +21,10 @@ def on_connected(worker, addr):
         g.set_conn_meta('client_addr', addr[0])
 
 
-def _install_signal_handlers():
-    signal.handle_ttin()
-
-
 def post_fork(arbitor, worker):
     worker.app.chdir()
     from ..env import initialize
     initialize()
-    _install_signal_handlers()
 
 
 def when_ready(arbitor):
