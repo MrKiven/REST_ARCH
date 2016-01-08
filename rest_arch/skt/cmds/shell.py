3ad3fc6f5dc2d12bb71ba97c77c99f16725bca1c
kiven <kiven.mr@gmail.com>
2016-01-08 14:39:25 +0800

    add http client for `skt shell` call

--- rest_arch/skt/cmds/shell.py
+++ rest_arch/skt/cmds/shell.py
@@ -3,6 +3,10 @@
 import sys
 import contextlib
 import click
+from ..consts import DEFAULT_APP_PORT
+from ...client import Client
+
+import __main__
 
 
 @contextlib.contextmanager
@@ -13,14 +17,18 @@ def _client_ctx(app_name, service, host, port):
 @click.command()
 @click.option('-h', '--host', default='localhost',
               help='service host(default localhost)')
-@click.option('-p', '--port', default=None, help='service port')
+@click.option('-p', '--port', default=DEFAULT_APP_PORT, help='service port')
 @click.option('-r', '--rpc', default=False, is_flag=True,
               help='if use rpc connect')
 @click.option('--profile', default=False, is_flag=True,
               help='Profile all apis calls')
 def shell(host, port, rpc, profile):
-    def embed_with_cli():
+    user_ns = __main__.__dict__
+    c = Client(host, port)
+
+    def embed_with_cli(client):
         import IPython
         del sys.exitfunc
-        IPython.embed()
-    embed_with_cli()
+        user_ns.update(dict(c=client))
+        IPython.embed(user_ns=user_ns)
+    embed_with_cli(c)
