c0a941dfe37597f8aed508f14ef8eb9127ce21b0
kiven <kiven.mr@gmail.com>
2016-01-13 13:40:15 +0800

    add db comment

---
+++ rest_arch/db.py
@@ -0,0 +1,246 @@
+# -*- coding: utf-8 -*-
+
+import os
+import time
+import threading
+import random
+import gevent
+import uuid
+import sha
+import logging
+import functools
+
+from sqlalchemy import event
+from sqlalchemy import create_engine as sqlalchemy_create_engine
+from sqlalchemy.orm import Session, scoped_session, sessionmaker
+from sqlalchemy.exc import SQLAlchemyError
+from sqlalchemy.ext.declarative import declarative_base, DeclarativeMeta
+
+from .skt.env import is_in_dev
+from .log import get_sql_logger
+from .conf import settings
+from .ctx import g
+
+db_ctx = threading.local()
+logger = logging.getLogger(__name__)
+
+
+def scope_func():
+    if not hasattr(db_ctx, 'session_stack'):
+        db_ctx.session_stack = 0
+    return (threading.current_thread().ident, db_ctx.session_stack)
+
+
+def close_connections(engines, transations):
+    if engines and transations:
+        for engine in engines:
+            for parent in transations:
+                conn = parent._connections.get(engine)
+                if conn:
+                    conn = conn[0]
+                    conn.invalidate()
+
+
+class RoutingSession(Session):
+    _name = None
+
+    def __init__(self, engines, *args, **kwds):
+        super(RoutingSession, self).__init__(*args, **kwds)
+        self.engines = engines
+        self.slave_engines = [e for role, e in engines.items()
+                              if role != 'master']
+        assert self.slave_engines, ValueError("DB slave configs is wrong!")
+        self._id = self.gen_id()
+        get_sql_logger().ctx.current_session_id = self._id
+
+    def get_bind(self, mapper=None, clause=None):
+        if self._name:
+            return self.engines[self._name]
+        elif self._flushing:
+            return self.engines['master']
+        else:
+            return random.choice(self.slave_engines)
+
+    def using_bind(self, name):
+        self._name = name
+        return self
+
+    def rollback(self):
+        with gevent.Timeout(5):
+            super(RoutingSession, self).rollback()
+
+    def close(self):
+        current_transations = tuple()
+        if self.transaction is not None:
+            current_transations = self.transaction._iterate_parents()
+        try:
+            with gevent.Timeout(5):
+                super(RoutingSession, self).close()
+        # pylint: disable=E0712
+        except gevent.Timeout:
+            # pylint: enable=E0712
+            close_connections(self.engines.itervalues(), current_transations)
+            raise
+
+    def gen_id(self):
+        pid = os.getpid()
+        tid = threading.current_thread().ident
+        clock = time.time() * 1000
+        address = id(self)
+        hash_key = self.hash_key
+        return sha.new('{0}\0{1}\0{2}\0{3}\0{4}'.format(
+            pid, tid, clock, address, hash_key)).hexdigest()[:20]
+
+
+def patch_engine(engine):
+    pool = engine.pool
+    pool._origin_recyle = pool._recycle
+    del pool._recycle
+    setattr(pool.__class__, '_recycle', RecycleField())
+    return engine
+
+
+def create_engine(*args, **kwds):
+    engine = patch_engine(sqlalchemy_create_engine(*args, **kwds))
+    get_sql_logger().register(engine)
+    event.listen(engine, 'before_cursor_execute', sql_commenter, retval=True)
+    return engine
+
+
+def gen_commit_deco(DBSession, raise_exc, error_code):
+    def decorated(func):
+        @functools.wraps(func)
+        def wrapper(*args, **kwargs):
+            session = DBSession()
+            try:
+                ret = func(*args, **kwargs)
+                session.flush()
+                if not session.identity_map.values():
+                    logger.warning("Session may have been closed")
+                session.commit()
+            except SQLAlchemyError as e:
+                session.rollback()
+                raise_exc(error_code, repr(e))
+            finally:
+                session.close()
+            return ret
+        return wrapper
+    return decorated
+
+
+def make_session(engines, force_scope=False, info=None):
+    if is_in_dev() or force_scope:
+        scopefunc = scope_func
+    else:
+        scopefunc = None
+
+    session = scoped_session(
+        sessionmaker(
+            class_=RoutingSession,
+            expire_on_commit=False,
+            engines=engines,
+            info=info or {"name": uuid.uuid4().hex},
+        ),
+        scopefunc=scopefunc
+    )
+    return session
+
+
+def sql_commenter(conn, cursor, statement, params, context, executemany):
+    if hasattr(g, 'call_meta_data') and 'request_id' in g.call_meta_data:
+        request_id = g.get_call_meta('request_id')
+        rpc_id = g.get_call_meta('seq')
+        role = conn._execution_options.get('role', 'unknown')
+        statement = "/* E:rid=%s&rpcid=%s&role=%s:E */ %s" % (
+            request_id,
+            rpc_id,
+            role,
+            statement
+        )
+    return statement, params
+
+
+class ModelMeta(DeclarativeMeta):
+    def __new__(self, name, bases, attrs):
+        cls = DeclarativeMeta.__new__(self, name, bases, attrs)
+
+        from .cache import CacheMixinBase
+        for base in bases:
+            if issubclass(base, CacheMixinBase) and hasattr(cls, "_hook"):
+                cls._hook.add(cls)
+                break
+        return cls
+
+
+def model_base():
+    return declarative_base(metaclass=ModelMeta)
+
+
+class RecycleField(object):
+    def __get__(self, instance, klass):
+        if instance is not None:
+            return int(random.uniform(0.75, 1) * instance._origin_recyle)
+        raise AttributeError
+
+
+class DBManager(object):
+    def __init__(self):
+        self.session_map = {}
+
+    def create_sessions(self):
+        for db, db_configs in settings.DB_SETTINGS.iteritems():
+            self.add_session(db, db_configs)
+
+    def get_session(self, name):
+        try:
+            return self.session_map[name]
+        except KeyError:
+            raise KeyError(
+                '`%s` session not created, check `DB_SETTINGS`' % name)
+
+    def add_session(self, name, config):
+        if name in self.session_map:
+            raise ValueError("Duplicate session name {},"
+                             "please check your config".format(name))
+        session = self._make_session(name, config)
+        self.session_map[name] = session
+
+    @classmethod
+    def _make_session(cls, db, config):
+        urls = config['urls']
+        pool_size = config.get('pool_size', settings.DB_POOL_SIZE)
+        max_overflow = config.get(
+            'max_overflow', settings.DB_MAX_OVERFLOW)
+        pool_recycle = config.get(
+            'pool_recycle', settings.DB_POOL_RECYCLE)
+        engines = {
+            role: cls.create_engine(
+                dsn, pool_size=pool_size, max_overflow=max_overflow,
+                pool_recycle=pool_recycle, execution_options={'role': role})
+            for role, dsn in urls.iteritems()
+        }
+        return make_session(engines, info={"name": db})
+
+    def close_sessions(self, should_close_connection=False):
+        dbsessions = self.session_map
+        for dbsession in dbsessions.itervalues():
+            if should_close_connection:
+                session = dbsession()
+                if session.transaction is not None:
+                    close_connections(session.engines.itervalues(),
+                                      session.transaction._iterate_parents())
+            try:
+                dbsession.remove()
+            except:
+                logger.exception("Error closing session")
+
+    @classmethod
+    def create_engine(cls, *args, **kwds):
+        engine = patch_engine(sqlalchemy_create_engine(*args, **kwds))
+        get_sql_logger().register(engine)
+        event.listen(engine, 'before_cursor_execute', sql_commenter,
+                     retval=True)
+        return engine
+
+db_manager = DBManager()
+db_manager.create_sessions()
--- rest_arch/log.py
+++ rest_arch/log.py
@@ -1,9 +1,19 @@
 # -*- coding: utf-8 -*-
 
 import logging
+import os
+import time
 import contextlib
+from sqlalchemy import event
+import threading
+
 from .skt.config import load_app_config
+from .skt import env
 from .ctx import g
+from .consts import PLACE_HOLDER
+from .conf import settings
+
+LIB_DIR_PATH = os.path.dirname(os.path.abspath(__file__))
 
 
 def obj2str(obj):
@@ -80,3 +90,190 @@ class RestFormatter(logging.Formatter):
     def format(self, record):
         record.msg = self._format(record.msg)
         return super(RestFormatter, self).format(record)
+
+
+#########
+# SQL Logging
+#########
+
+
+class SQLLogger(object):
+    """SQL logger listens sqlalchemy events on ``engines`` and emits
+    logging after ``cursor`` executed::
+
+        sql_logger = SQLLogger()
+        sql_logger.register(engine)
+    """
+    # senstive db field list
+    _path = os.path.join(LIB_DIR_PATH, 'static', 'dbsens')
+    _content = open(_path).read()
+    DB_SENSFIELDS = set(filter(None, _content.splitlines()))
+
+    def __init__(self, name='zeus_core.sql_logger'):
+        self.logger = logging.getLogger(name=name)
+        env.require('gevent_patched')
+        self.ctx = threading.local()
+        self.ctx.disable_sql_logging = False
+        self.ctx.sql_start_time = None
+        self.switch = lambda: False
+
+    def use_switch(self, fn):
+        """
+        Usage::
+
+            sql_logger.use_switch(lambda: True)  # on
+        """
+        self.switch = fn
+
+    def register(self, engine):
+        event.listen(engine, 'before_cursor_execute',
+                     self.before_cursor_execute)
+        event.listen(engine, 'after_cursor_execute',
+                     self.after_cursor_execute)
+        event.listen(engine, 'dbapi_error',
+                     self.handle_dbapi_error)
+        event.listen(engine, 'commit', self.on_commit)
+        event.listen(engine, 'rollback', self.on_rollback)
+        event.listen(engine, 'connect', self.on_connect)
+
+    @contextlib.contextmanager
+    def disable_sql_logging(self):
+        """
+        Usage example::
+
+            with sql_logger.disable_sql_logging():
+                session.add(User)
+                session.commit()
+        """
+        self.ctx.disable_sql_logging = True
+        try:
+            yield self
+        finally:
+            self.ctx.disable_sql_logging = False
+
+    def before_cursor_execute(self, conn, cursor, statement,
+                              params, context, executemany):
+        if not hasattr(self.ctx, 'disable_sql_logging'):
+            self.ctx.disable_sql_logging = False
+        self.ctx.sql_start_time = time.time()
+
+    def after_cursor_execute(self, conn, cursor, statement,
+                             params, context, executemany):
+        self._do_logging(self._get_logging_ctx(conn), statement,
+                         params=params, exc=None)
+
+    def handle_dbapi_error(self, conn, cursor, statement,
+                           params, context, exception):
+        self._do_logging(self._get_logging_ctx(conn), statement,
+                         params=params, exc=exception)
+
+    def on_commit(self, conn):
+        self._do_logging(self._get_logging_ctx(conn), 'COMMIT')
+
+    def on_rollback(self, conn):
+        self._do_logging(self._get_logging_ctx(conn), 'ROLLBACK')
+
+    def on_connect(self, dbapi_conn, conn_record):
+        self._do_logging(self._get_logging_ctx_by_dbapi_conn(dbapi_conn),
+                         'Connection Open')
+
+    def _get_logging_ctx(self, conn):
+        return self._get_logging_ctx_by_dbapi_conn(conn.connection.connection,
+                                                   driver=conn.engine.driver)
+
+    def _get_logging_ctx_by_dbapi_conn(self, dbapi_conn, driver=None):
+        default = {}.fromkeys(['db', 'host', 'port', 'autocommit'],
+                              PLACE_HOLDER)
+
+        if dbapi_conn is None:
+            return default
+
+        if driver is None:
+            driver = dbapi_conn.__class__.__module__.split('.')[0]
+
+        if driver == 'pymysql':
+            return dict(db=dbapi_conn.db,
+                        host=dbapi_conn.host,
+                        port=dbapi_conn.port,
+                        autocommit=dbapi_conn.get_autocommit())
+        elif driver == 'psycopg2':
+            dct = {}
+            for item in dbapi_conn.dsn.split():
+                lst = item.split('=')
+                dct[lst[0]] = lst[1]
+            return dict(db=dct.get('dbname', PLACE_HOLDER),
+                        host=dct.get('host', PLACE_HOLDER),
+                        port=dct.get('port', PLACE_HOLDER),
+                        autocommit=dbapi_conn.autocommit)
+        return default
+
+    def _do_logging(self, ctx, statement, params=None, exc=None):
+        if not hasattr(self, 'switch') or (
+                callable(self.switch) and not self.switch()):
+            # there's not a switch or there's a switch and it gives us `off`
+            return
+
+        if getattr(self.ctx, 'disable_sql_logging', False):
+            # switch is `on` but logging is temply disabled for
+            # current thread.
+            return
+
+        meta = {}
+
+        # time cost
+        cost = None
+        if getattr(self.ctx, 'sql_start_time', None) is not None:
+            cost = time.time() - self.ctx.sql_start_time
+            self.ctx.sql_start_time = None
+
+        if cost is None:
+            meta['cost'] = PLACE_HOLDER
+        else:
+            meta['cost'] = '%.2fms' % (cost * 1000)
+
+        # db instance
+        meta['db'] = '{0}/{1}'.format(ctx.get('host', PLACE_HOLDER),
+                                      ctx.get('db', PLACE_HOLDER))
+        # autocommit
+        if 'autocommit' in ctx and ctx['autocommit'] in (False, True):
+            meta['ac'] = int(ctx['autocommit'])
+        else:
+            meta['ac'] = PLACE_HOLDER
+        # result
+        meta['res'] = exc or 'ok'
+        # session id
+        meta['session'] = getattr(self.ctx, 'current_session_id', PLACE_HOLDER)
+
+        # filter senstive fields
+        statement_list = statement.split()
+        has_sens_field = set(statement_list) & SQLLogger.DB_SENSFIELDS
+        statement_ = obj2str(' '.join(statement_list))
+
+        if has_sens_field or params is None:
+            params_ = PLACE_HOLDER
+        else:
+            params_ = obj2str(params)
+
+        content = '{0} ## {1}'.format(statement_, params_)
+
+        with logging_meta(**meta):
+            if exc is None:
+                if cost is None or cost < 1:
+                    self.logger.info(content)
+                elif 1 <= cost < 5:
+                    self.logger.warning(content)
+                else:
+                    self.logger.error(content)
+            else:
+                self.logger.error(content)
+
+sql_logger = None
+
+
+def get_sql_logger():
+    global sql_logger
+    if sql_logger is None:
+        sql_logger = SQLLogger()
+        env.require('settings_updated')
+        sql_logger.use_switch(settings.SQLLOGGER_SWITCH)
+    return sql_logger
